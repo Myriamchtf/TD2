00c542b54cee561def0b02b0da9d1541b7f4eabb
Myriamchtf <91917709+Myriamchtf@users.noreply.github.com>
2021-10-19 20:42:27 +0200

    Create MouseController.java

---
+++ MouseController.java
@@ -0,0 +1,15 @@
+package controller;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class MouseController extends MouseAdapter implements MouseMotionListener{
+
+	public MouseController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		
+	}
+}
